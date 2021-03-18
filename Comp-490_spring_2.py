ad222ba8e8105b08fed9c41804f2a15200bf6951
Electronick79 <63924606+Electronick79@users.noreply.github.com>
2021-03-18 12:17:08 -0400

    Add files via upload

---
+++ Comp-490_spring_2.py
@@ -0,0 +1,121 @@
+import mysql.connector
+import mysql.connector
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+
+connection = create_server_connection("localhost", "root", pw)
+
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+
+def create_db_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+
+mycursor.excute("CREATE TABLE studentschool((200),(3),(48),"
+                "(),(),"
+                "2016.repayment.3_yr_repayment.overall()")
+for tb in mycursor:
+    create_school.name = """
+    CREATE TABLE school.name (
+      school.name INT PRIMARY KEY,
+      school.name VARCHAR() NOT NULL,
+      address VARCHAR(60) NOT NULL,
+      school.name VARCHAR(20)
+    );
+     """
+    create_school.city = """
+     CREATE TABLE school.city (
+       school.city_id INT PRIMARY KEY,
+       school.city VARCHAR() NOT NULL,
+       school.city VARCHAR() NOT NULL,
+       school.city_phone VARCHAR(6),
+       school.city INT
+     );
+     """
+    create_2020.student.size = """
+     CREATE TABLE 2020.student.size (
+       2020.student.size INT PRIMARY KEY,
+       2020.student.size VARCHAR() NOT NULL,
+       020.student.size VARCHAR() NOT NULL,
+       level VARCHAR(2),
+       2020.student.size INT,
+
+     );
+     """
+    create_2018student.size = """
+          CREATE TABLE 2018student.size (
+            2018student.size_id INT PRIMARY KEY,
+            2018student.size VARCHAR() NOT NULL,
+            2018student.size VARCHAR() NOT NULL,
+
+          );
+          """
+    create_2017.earnings_3_yrs_after_completion.overall_count_over_poverty_line = """
+        CREATE TABLE 2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line (
+          2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line INT PRIMARY KEY,
+          2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line VARCHAR(40) NOT NULL,
+          2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line VARCHAR(20)
+        );
+         """
+    create_2016.repayment_3_yr_repayment.overall = """
+            CREATE TABLE 2016.repayment.3_yr_repayment.overall (
+            2016.repayment.3_yr_repayment.overall INT PRIMARY KEY,
+            2016.repayment.3_yr_repayment.overall VARCHAR(40) NOT NULL,
+            2016.repayment.3_yr_repayment.overall VARCHAR(40) NOT NULL,
+
+          );
+          """
+
+    connection = create_db_connection("localhost", "root", pw, db)
+    execute_query(connection, create_school.name)
+    execute_query(connection, create_school.city)
+    execute_query(connection, create_2020.student.size)
+
+    execute_query(connection, create_2018student.size)
+    execute_query(connection, create_2017.earnings_3_yrs_after_completion.overall_count_over_poverty_line)
+    execute_query(connection, create_2016.repayment_3_yr_repayment.overall)
+
+
---
+++ comp_490_spring_3.py
@@ -0,0 +1,84 @@
+import sqlite3
+
+import mysql as mysql
+import pandas as pd
+
+# template that you can use to connect Python to SQL Server
+import pyodbc
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=server_name;'
+                      'Database=database_name;'
+                      'Trusted_Connection=yes;')
+
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM database_name.table')
+
+for row in cursor:
+    print(row)
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+  )
+# creating database_cursor to perform SQL operation
+db_cursor = db_connection.cursor()
+# executing cursor with execute method and pass SQL query
+db_cursor.execute("CREATE DATABASE comp490_spring_3")
+# get list of all databases
+db_cursor.execute("SHOW DATABASES")
+#print all databases
+for db in db_cursor:
+    print(db)
+
+df = pd.read_xlsx(r'Path where the XLSX file is stored\COMP490_SPRING_3.XLSX')
+print(df)
+
+df = pd.read_xlsx(r'C:\Users\Electronick\OneDrive\Desktop\COMP_490\COMP490_SPRING_3.xlsx')   #read the csv file (put 'r' before the path string to address any special characters in the path, such as '\').
+# Don't forget to put the file name at the end of the path + ".xlsx or csv"
+print(df)
+
+conn = sqlite3.connect('TestDB.db')
+c = conn.cursor()
+
+# Create table - occupation occupational
+c.execute('''CREATE TABLE OCCUPATIONAL
+             ([generated_id] INTEGER PRIMARY KEY,[Occupational_Name] text, [Title] text, [Date_start] dates, [Occupation] occupation, [Wage] integer, [State] text,,)''')
+
+db_cursor.execute("SHOW TABLES")
+for table in db_cursor:
+ print(table)
+
+# Create table - STATES
+c.execute('''CREATE TABLE STATES
+             ([generated_id] INTEGER PRIMARY KEY,[States_ID] integer, [States_Name] text)''')
+
+db_cursor.execute("SHOW TABLES")
+for table in db_cursor:
+ print(table)
+
+# Create table - EMPLOYMENT
+c.execute('''CREATE TABLE EMPLOYMENT_STATUS
+             ([Employment_Name] text, [STATE_Name] text, [Date] date), [ Field_in_that_state] text''')
+db_cursor.execute("SHOW TABLES")
+
+for table in db_cursor:
+ print(table)
+
+# Create table - 25TH_PERCENTILE_SALARY
+c.execute('''CREATE TABLE 25TH_PERCENTILE_SALARY
+             ([25th_percent_salary] integer, [Employment_Name] text, [Date] date), [ Field_in_that_state] text''')
+
+db_cursor.execute("SHOW TABLES")
+for table in db_cursor:
+ print(table)
+
+
+conn.commit()
+
+
+
+
+
+
