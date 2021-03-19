2b4323e6c956dcfb45d9691944fbc9193d8e8a19
Electronick79 <63924606+Electronick79@users.noreply.github.com>
2021-03-19 01:51:58 -0400

    just try it again

--- comp_490_spring_4.py
+++ comp_490_spring_4.py
@@ -3,11 +3,15 @@ from tkinter.messagebox import *
 
 import pandas as pd
 import numpy as np
+import geopandas
+import math
+import geopandas
+import json
 
+# When your program first starts up, with the python GUI, allow the user to choose to either
+# update the data run the data visualization
 
-
-
-class Notepad:
+class excel:
 
     __root = Tk()
 
@@ -120,15 +124,47 @@ class Notepad:
         self.__thisTextArea.config(yscrollcommand=self.__thisScrollBar.set)
 
         # ////////////////////////////////////////////////////////////////////////////////////////////////
+        # When your program first starts up, with the python GUI, allow the user to choose to either
+        # update the data run the data visualization
+        # https://towardsdatascience.com/how-to-create-an-interactive-geographic-map-using-python
+        # -and-bokeh-12981ca0b567
+        from bokeh.io import output_notebook, show, output_file
+        from bokeh.plotting import figure
+        from bokeh.models import GeoJSONDataSource, LinearColorMapper, ColorBar, NumeralTickFormatter
+        from bokeh.palettes import brewer
 
-        from bokeh.io import output_file, output_notebook
-        from bokeh.plotting import figure, show
-        from bokeh.models import ColumnDataSource
-        from bokeh.layouts import row, column, gridplot
-        from bokeh.models.widgets import Tabs, Panel
+        from bokeh.io.doc import curdoc
+        from bokeh.models import Slider, HoverTool, Select
+        from bokeh.layouts import widgetbox, row, column
 
+        # Read the geojson map file for Realtor Neighborhoods into a GeoDataframe object
+        sf = geopandas.read_file('https://raw.githubusercontent.com/JimKing100/SF_Real_Estate_Live/master'
+                                 '/data/Realtor%20Neighborhoods.geojson')
 
+        # Set the Coordinate Referance System (crs) for projections
+        # ESPG code 4326 is also referred to as WGS84 lat-long projection
+        sf.crs = {'init': 'epsg:4326'}
+        # Rename columns in geojson map file
+        sf = sf.rename(columns={'geometry': 'geometry', 'nbrhood': 'neighborhood_name',
+                                'nid': 'subdist_no'}).set_geometry('geometry')
 
+        # Change neighborhood id (subdist_no) for correct code for Mount Davidson Manor and for parks
+        sf.loc[sf['neighborhood_name'] == 'Mount Davidson Manor', 'subdist_no'] = '4n'
+        sf.loc[sf['neighborhood_name'] == 'Golden Gate Park', 'subdist_no'] = '12a'
+        sf.loc[sf['neighborhood_name'] == 'Presidio', 'subdist_no'] = '12b'
+        sf.loc[sf['neighborhood_name'] == 'Lincoln Park', 'subdist_no'] = '12c'
+
+        sf.sort_values(by=['subdist_no'])
+
+        # Determine where the visualization will be rendered
+        output_file('filename.html')
+        output_notebook()  # Render inline in a Jupyter Notebook
+
+        # Set up the figure(s)
+        fig = figure()
+        show(fig)
+
+        df = pd.read_xlsx(r'C:\Users\Electronick\OneDrive\Desktop\COMP_490\COMP490_SPRING_3.xlsx')
         # Create an empty string called ticker_string
         ticker_string = ''
         # Loop through every element of `tickers` and add them and a comma to ticker_string
@@ -142,7 +178,31 @@ class Notepad:
         endpoints = 'chart'
         years = '5'
 
-        df = pd.read_xlsx(r'C:\Users\Electronick\OneDrive\Desktop\COMP_490\COMP490_SPRING_3.xlsx')
+
+        #   When updating the data: let the user choose the file name for the excel file
+        def writer(header, data, filename, option):
+            with open(filename, "w", newline="") as xlsxfile:
+                if option == "write":
+
+                    movies = xlsxfile.writer(csvfile)
+                    movies.writerow(header)
+                    for x in data:
+                        movies.writerow(x)
+                elif option == "update":
+                    writer = xlsx.DictWriter(csvfile, fieldnames=header)
+                    writer.writeheader()
+                    writer.writerows(data)
+                else:
+                    print("Option is not known")
+
+                    # The data visualization: provide the user the ability to do two forms of data analysis,The first
+                    # analysis.should display the data in a color coded text format as a list in ascending or descending
+                    # order (let the user choose) and the second should render a map to visualize the data.
+
+                    iris = pd.read_xlsx('iris.xlsx',
+                                        names=[ "school.city_id INT PRIMARY KEY, school.city VARCHAR() NOT NULL, school.city "
+                                            "VARCHAR() NOT NULL, school.city_phone VARCHAR(6), school.city INT"])
+                    print(iris.head())
         # &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
     def __quitApplication(self):
@@ -189,7 +249,6 @@ class Notepad:
                                             defaultextension=".xlsx",
                                             filetypes=[("All Files", "*.*"),
                                                        ("Exel Documents", "*.xlsx")])
-
             if self.__file == "":
                 self.__file = None
             else:
@@ -223,7 +282,5 @@ class Notepad:
         self.__root.mainloop()
 
     # Run main application
-
-
-excel = Excel(width=600, height=400)
+excel = excel(width=600, height=400)
 excel.run()
